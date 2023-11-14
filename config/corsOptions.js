559c5362a9433e2dbab9a6d24a496c5873883322
Rupet-Osakwe <rupetosakwe@gmail.com>
2023-11-14 21:16:23 +0100

    made changes on corsOption config to alow http mtd

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,12 +2,14 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
+        const origins = allowedOrigins();
+        if (origins.includes(origin) || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not Allowed By CORS'));
         }
     },
+    methods: 'GET,HEAD,PUT,POST,DELETE',
     optionsSuccessStatus: 200
 };
 
