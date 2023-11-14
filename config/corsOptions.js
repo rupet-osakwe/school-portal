39a220126b0862c25d3923451d5d0a62671992e4
Rupet-Osakwe <rupetosakwe@gmail.com>
2023-11-14 12:05:02 +0100

    changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,14 +2,13 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
         } else {
             callback(new Error('Not Allowed By CORS'));
         }
-
     },
     optionsSuccessStatus: 200
-}
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
