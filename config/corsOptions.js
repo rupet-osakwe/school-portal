05baa67f979dc3ddd7c3b4ca6d4ab5a7ba90f22e
Rupet-Osakwe <rupetosakwe@gmail.com>
2023-11-17 06:58:56 +0100

    changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,21 +1,23 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-            return
-        } else {
-            callback(new Error('Not Allowed By CORS'));
-        }
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        next();
-        return origin
-    },
+    origin: 'https://schoolportal-q4sz.onrender.com',
+    //  (origin, callback) => {
+    //     if (allowedOrigins.includes(origin) || !origin) {
+    //         callback(null, true);
+    //         return
+    //     } else {
+    //         callback(new Error('Not Allowed By CORS'));
+    //     }
+    //     res.header('Access-Control-Allow-Origin', '*');
+    //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    //     next();
+    //     return origin
+    // },
 
     // methods: 'GET,PUT,POST,DELETE',
+    credentials: true,
     methods: `*`,
     optionsSuccessStatus: 200
 };
