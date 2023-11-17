9014ecf1ca7f4b6a6954c44aada33c13916dc343
Rupet-Osakwe <rupetosakwe@gmail.com>
2023-11-17 07:11:27 +0100

    removed headers in corsOption

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,23 +1,21 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: 'https://schoolportal-q4sz.onrender.com',
-    //  (origin, callback) => {
-    //     if (allowedOrigins.includes(origin) || !origin) {
-    //         callback(null, true);
-    //         return
-    //     } else {
-    //         callback(new Error('Not Allowed By CORS'));
-    //     }
-    //     res.header('Access-Control-Allow-Origin', '*');
-    //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    //     next();
-    //     return origin
-    // },
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+            return
+        } else {
+            callback(new Error('Not Allowed By CORS'));
+        }
+        //     res.header('Access-Control-Allow-Origin', '*');
+        // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // next();
+    },
 
-    // methods: 'GET,PUT,POST,DELETE',
-    credentials: true,
+    methods: 'GET,PUT,POST,DELETE',
+    // credentials: true,
     methods: `*`,
     optionsSuccessStatus: 200
 };
