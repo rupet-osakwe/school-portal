23c1d4ecce5572acb42d58cb155b8ef66dee6a86
Rupet-Osakwe <rupetosakwe@gmail.com>
2023-11-20 00:35:29 +0100

    made some changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,22 +1,44 @@
+// const allowedOrigins = require('./allowedOrigins');
+
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         if (allowedOrigins.includes(origin) || !origin) {
+//             callback(null, true);
+//             return
+//         } else {
+//             callback(new Error('Not Allowed By CORS'));
+//         }
+
+//         // req.header('Access-Control-Allow-Origin', '*');
+//         // req.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//         // req.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//         // req.header('Access-Control-Request-Headers', 'Content-Type, Authorization');
+//         req.header('Access-Control-Allow-Credentials': true)
+
+//         next();
+//     },
+//     Access-Control-request-Method:"POST",
+//     methods: 'GET,PUT,POST,DELETE',
+//     credentials: true,
+//     // methods: `*`,
+//     optionsSuccessStatus: 200
+// };
+
+// module.exports = corsOptions;
+
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
-            return
+            return;
         } else {
             callback(new Error('Not Allowed By CORS'));
         }
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        next();
     },
-
     methods: 'GET,PUT,POST,DELETE',
-    // credentials: true,
-    methods: `*`,
+    credentials: true,
     optionsSuccessStatus: 200
 };
 
--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,4 +1,23 @@
 
+// const allowedOrigins = require('../config/allowedOrigins');
+
+// const credentials = (req, res, next) => {
+//     const origin = req.headers.origin;
+
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//         return res.status(200).json({});
+//     }
+//     if (allowedOrigins.includes(origin)) {
+//         res.header('Access-Control-Allow-Credentials', true);
+//     }
+
+//     next();
+// }
+
+// module.exports = credentials;
+
 const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
@@ -9,11 +28,15 @@ const credentials = (req, res, next) => {
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         return res.status(200).json({});
     }
+
     if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.header('Access-Control-Allow-Credentials', true);
     }
-
     next();
-}
+};
 
 module.exports = credentials;
+
